aa0a509a801e81462a00da7b8b45acae89d96325
Anupam Upadhyay <anupamupadhyay@devnexus.in>
2025-12-02 14:21:12 +0530

    update with about us

--- src/Pages/main/Home.jsx
+++ src/Pages/main/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import HomeHero from '../../Components/Home/HomeHero'
-import FindDreamHouse from '../../Components/Form/FindDreamHouse'
+import Aboutus from '../../Components/AboutUs'
 
 const Home = () => {
     return (
-        <div>
+        <div className='flex flex-col gap-3 items-start'>
             <HomeHero />
+            <section className='flex items-center mx-auto w-full max-w-6xl p-4'>
+
+            <Aboutus />
+            </section>
         </div>
     )
 }
