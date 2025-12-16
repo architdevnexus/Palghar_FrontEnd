3dbc3babe144dd41790253117b58a178f5022120
Anupam-Devnexus <anupamupadhyay@devnexus.in>
2025-12-16 17:35:09 +0530

    update

--- src/Pages/main/Home.jsx
+++ src/Pages/main/Home.jsx
@@ -1,44 +1,102 @@
-import React from 'react'
-import HomeHero from '../../Components/Home/HomeHero'
-import Aboutus from '../../Components/AboutUs'
-import NewProjectsSlider from '../../Components/Sliders/NewProjectsSlider'
-import ProjectsTabs from '../../Components/ProjectShowCase'
-import ExploreNewCity from '../../Components/BentoGrid/ExploreNewCity'
-import HomeMap from '../../Components/Maps/HomeMap'
-import TestimonialsSlider from '../../Components/Testimonial'
-import WhatWeDo from '../../Components/WhatWeDo'
-import FAQ from '../../Components/FAQ'
-import GetInTouch from '../../Components/Form/GetInTouch'
-import HomeDream from "../../Components/Home/HomeDream"
-import WhatWeHaveDone from '../../Components/Home/WhatWeHaveDone'
-import Road_Alignment from '../../Components/Road_Alignment'
-import Parivar from '../../Components/Parivar'
+import React, { lazy, Suspense } from "react";
 
+/* ===========================
+   Lazy Loaded Sections
+=========================== */
+const HomeHero = lazy(() => import("../../Components/Home/HomeHero"));
+const Aboutus = lazy(() => import("../../Components/AboutUs"));
+const NewProjectsSlider = lazy(() =>
+  import("../../Components/Sliders/NewProjectsSlider")
+);
+const ProjectsTabs = lazy(() =>
+  import("../../Components/ProjectShowCase")
+);
+const ExploreNewCity = lazy(() =>
+  import("../../Components/BentoGrid/ExploreNewCity")
+);
+const HomeDream = lazy(() =>
+  import("../../Components/Home/HomeDream")
+);
+const WhatWeHaveDone = lazy(() =>
+  import("../../Components/Home/WhatWeHaveDone")
+);
+const RoadAlignment = lazy(() =>
+  import("../../Components/Road_Alignment")
+);
+const Parivar = lazy(() =>
+  import("../../Components/Parivar")
+);
+const TestimonialsSlider = lazy(() =>
+  import("../../Components/Testimonial")
+);
+const WhatWeDo = lazy(() =>
+  import("../../Components/WhatWeDo")
+);
+const FAQ = lazy(() =>
+  import("../../Components/FAQ")
+);
+const GetInTouch = lazy(() =>
+  import("../../Components/Form/GetInTouch")
+);
+
+/* ===========================
+   Fallback Loader
+=========================== */
+const SectionLoader = () => (
+  <div className="w-full h-40 flex items-center justify-center">
+    <div className="animate-pulse h-6 w-32 bg-gray-200 rounded" />
+  </div>
+);
+
+/* ===========================
+   HOME PAGE
+=========================== */
 const Home = () => {
-    return (
-        <div className='flex flex-col gap-3  w-full'>
-            <HomeHero />
-            <section className='flex items-center mx-auto w-full max-w-6xl p-4'>
+  return (
+    <main className="flex flex-col gap-6 w-full overflow-hidden">
 
-                <Aboutus />
-            </section>
+      <Suspense fallback={<SectionLoader />}>
+        <HomeHero />
+      </Suspense>
 
-            <NewProjectsSlider />
-            <ProjectsTabs />
-            <ExploreNewCity />
-            {/* <HomeMap /> */}
-            <HomeDream />
-            <WhatWeHaveDone />
-            <img src="/DevelopmentPath.svg" alt="" srcset="" className='w-[90%] m-auto h-96' />
-            <Road_Alignment/>
-            <Parivar/>
-            <TestimonialsSlider />
-            <WhatWeDo />
-            <FAQ />
-            <GetInTouch />
+      <section className="w-full max-w-6xl mx-auto px-4">
+        <Suspense fallback={<SectionLoader />}>
+          <Aboutus />
+        </Suspense>
+      </section>
 
-        </div>
-    )
-}
+      <Suspense fallback={<SectionLoader />}>
+        <NewProjectsSlider />
+        <ProjectsTabs />
+        <ExploreNewCity />
+      </Suspense>
 
-export default Home
+      <Suspense fallback={<SectionLoader />}>
+        <HomeDream />
+        <WhatWeHaveDone />
+      </Suspense>
+
+      {/* Decorative Divider */}
+      <div className="w-full flex justify-center">
+        <img
+          src="/DevelopmentPath.svg"
+          alt="Development Path"
+          className="w-[96%] max-w-6xl object-contain"
+          loading="lazy"
+        />
+      </div>
+
+      <Suspense fallback={<SectionLoader />}>
+        <RoadAlignment />
+        <Parivar />
+        <TestimonialsSlider />
+        <WhatWeDo />
+        <FAQ />
+        <GetInTouch />
+      </Suspense>
+
+    </main>
+  );
+};
+
+export default Home;
